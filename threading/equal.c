8efcc65e96b6b23b2a8deef493599c5ecb90096b
John Detter <jdetter@wisc.edu>
2015-09-11 18:03:02 -0400

    equal is complete

--- threading/equal.c
+++ threading/equal.c
@@ -1,4 +1,37 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "bench.h"
+
+void* pthread_func(void* argument)
+{
+	printf("On the new thread!\n");
+
+	pthread_t cmp_1 = 0x12345678;
+	pthread_t cmp_2 = pthread_self();
+
+	uint val;
+	ull start = read_tscp(&val);	
+	pthread_equal(cmp_1, cmp_2);
+	ull end = read_tscp(&val);
+
+        printf("Start cycles: %llu\n", start);
+        printf("End cycles  : %llu\n", end);
+        printf("Difference  : %llu\n", (end - start));
+
+	return NULL;
+}
+
 int main(int argc, char** argv)
 {
+	pthread_t thr;
+
+	if(pthread_create(&thr, NULL, pthread_func, NULL ))
+		perror("");
+
+	pthread_join(thr, (void**)NULL);
 	return 0;
+
 }
